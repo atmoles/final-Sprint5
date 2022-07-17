a825ef82a06c11d105108829f245eef27c731bf2
Julian <105244530+Julianfried@users.noreply.github.com>
2022-07-17 19:40:27 -0300

    Update parseador.py

--- parseador.py
+++ parseador.py
@@ -1,21 +1,35 @@
 import json
-from .clases import cliente
-
+from clases import cliente
+from clases import direccion
+from clases import cuenta
+# lo q hace esta clase es parsearme el json para que lo pueda usar 
 class Parser():
+    #creo el constructor
     def __init__(self,file):
         self.file=file
+        #se ejecuta este metodo q defino para levantar el json
         self.load()
-        if self.data['tipo'] == 'BLACK':
-            self.cliente=cliente.ClienteBlack(self.data)
-        elif self.data['tipo'] == 'GOLD':
-            self.cliente=cliente.ClienteGold(self.data)
-        else:
-            self.cliente=cliente.ClienteClassic(self.data)
-        self.eventos=self.data['transacciones']
+        self.get_cliente()
+        self.eventos = self.data['transacciones']
+        # metodo para levantar el json,obtngo el diccionario parseado
         
     def load(self):
         f=open(self.file)
         self.data=json.load(f)
         f.close()
+    
+    def get_cliente(self):
+        if self.data['tipo'] == 'BLACK':
+            self.cliente=cliente.ClienteBlack(self.data)
+            self.cliente.crear_cuenta(cuenta.caja_de_ahorro_p)
+            self.cliente.crear_cuenta(cuenta.caja_de_ahorro_d)
+            self.cliente.crear_cuenta(cuenta.cuenta_corriente)
+        elif self.data['tipo'] == 'GOLD':
+            self.cliente=cliente.ClienteGold(self.data)
+            self.cliente.crear_cuenta(cuenta.caja_de_ahorro_p)
+            self.cliente.crear_cuenta(cuenta.caja_de_ahorro_d)
+        else:
+            self.cliente=cliente.ClienteClassic(self.data)
+            self.cliente.crear_cuenta(cuenta.caja_de_ahorro_p)
 
  
