24de0cad7bda98968c7b80c2f1d6bc7f35a2a4ab
Matias Laguna <80929109+MatiasLaguna@users.noreply.github.com>
2022-07-17 19:39:53 -0300

    Agrego class direccion

--- clases/direccion.py
+++ clases/direccion.py
@@ -1,2 +1,10 @@
 class Direccion():
-    pass
+    def __init__(self,data):
+        self.calle = data['calle']
+        self.numero = data['numero']
+        self.ciudad = data['ciudad']
+        self.provincia = data['provincia']
+        self.pais = data['pais']
+    
+    def get_direccion(self):
+        return self.calle + ' ' + self.numero + ' ' + self.ciudad
